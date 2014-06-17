8a8e18e2c32be55fabe28097432e5c2334742292
Justin Pilot <justin.pilot@cern.ch>
2014-06-17 14:23:42 -0500

    added cut to scripts

--- Summer14/bin/compareQuantities.C
+++ Summer14/bin/compareQuantities.C
@@ -1,4 +1,4 @@
-void compareQuantities(string filename1, string filename2, string var1, string var2, int nbins, float min, float max){
+void compareQuantities(string filename1, string filename2, string var1, string var2, int nbins, float min, float max, string cut = ""){
 
 
 	gStyle->SetOptStat(0);
@@ -16,8 +16,8 @@ void compareQuantities(string filename1, string filename2, string var1, string v
 
 	cout << var1 << "  " << var2 << endl;	
 
-	jetTree1->Draw(Form("%s>>var1H",var1.c_str()), "", "goff");
-	jetTree2->Draw(Form("%s>>var2H",var2.c_str()), "", "goff");
+	jetTree1->Draw(Form("%s>>var1H",var1.c_str()), cut.c_str(), "goff");
+	jetTree2->Draw(Form("%s>>var2H",var2.c_str()), cut.c_str(), "goff");
 
 	cout << var1H->GetEntries() << " " << var2H->GetEntries() << endl;
 
