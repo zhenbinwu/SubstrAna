55098c86e2d8cdcdec3abb549811a424f3557c79
zhenbinwu <zhenbinwu@gmail.com>
2014-08-17 17:18:50 -0500

    Add running version
    15min for 100 events!! Not good

--- Summer14/bin/runPuppi.cpp
+++ Summer14/bin/runPuppi.cpp
@@ -22,6 +22,7 @@
 #include "TTree.h"
 #include "TMath.h"
 
+#include <cstdlib>
 
 using namespace std;
 using namespace fastjet;
@@ -105,7 +106,7 @@ double unc( PseudoJet &iJet,JetCorrectionUncertainty *iJetUnc) {
 void setJet(PseudoJet &iJet,JetInfo &iJetI,std::vector<PseudoJet> &iParticles, bool iCHS,FactorizedJetCorrector *iJetCorr,JetCorrectionUncertainty *iJetUnc,JetCleanser &gsn_cleanser) {
     vector<PseudoJet> neutrals,chargedLV,chargedPU;
     getConstitsForCleansing(iJet.constituents(),neutrals,chargedLV,chargedPU);
-    PseudoJet     lClean = gsn_cleanser(neutrals,chargedLV,chargedPU);
+    //PseudoJet     lClean = gsn_cleanser(neutrals,chargedLV,chargedPU);
     
     // define safeAreaSub (PF)
     AreaDefinition area_def(active_area_explicit_ghosts,GhostedAreaSpec(SelectorAbsRapMax(5.0)));
@@ -124,17 +125,18 @@ void setJet(PseudoJet &iJet,JetInfo &iJetI,std::vector<PseudoJet> &iParticles, b
     if( iCHS) area_subtractor = new contrib::SafeAreaSubtractor(&bge_rho, &bge_rhom,SelectorIsPupCharged(),SelectorIsPupVertex());
     //iGMBE->set_particles(iParticles);
     PseudoJet lCorr =  (*area_subtractor)(iJet);
-    fastjet::Filter trimmer( fastjet::Filter(fastjet::JetDefinition(fastjet::kt_algorithm, 0.2), fastjet::SelectorPtFractionMin(0.05)));
-    PseudoJet lTrim     = (trimmer)(iJet);
-    trimmer.set_subtractor(area_subtractor);
-    PseudoJet lTrimSafe = (trimmer)(iJet);
-    JetMedianBackgroundEstimator bge_rhoC(rho_range,jet_def_for_rho, area_def);
-    BackgroundJetScalarPtDensity *scalarPtDensity = new BackgroundJetScalarPtDensity();
-    bge_rhoC.set_jet_density_class(scalarPtDensity);
-    bge_rhoC.set_particles(iParticles);
-    contrib::ConstituentSubtractor subtractor(&bge_rhoC);
-    subtractor.use_common_bge_for_rho_and_rhom(true);
-    PseudoJet lConstit = subtractor(iJet);
+
+    //fastjet::Filter trimmer( fastjet::Filter(fastjet::JetDefinition(fastjet::kt_algorithm, 0.2), fastjet::SelectorPtFractionMin(0.05)));
+    //PseudoJet lTrim     = (trimmer)(iJet);
+    //trimmer.set_subtractor(area_subtractor);
+    //PseudoJet lTrimSafe = (trimmer)(iJet);
+    //JetMedianBackgroundEstimator bge_rhoC(rho_range,jet_def_for_rho, area_def);
+    //BackgroundJetScalarPtDensity *scalarPtDensity = new BackgroundJetScalarPtDensity();
+    //bge_rhoC.set_jet_density_class(scalarPtDensity);
+    //bge_rhoC.set_particles(iParticles);
+    //contrib::ConstituentSubtractor subtractor(&bge_rhoC);
+    //subtractor.use_common_bge_for_rho_and_rhom(true);
+    //PseudoJet lConstit = subtractor(iJet);
 
     //Finally apply the JEC
     double lJEC = correction(iJet,iJetCorr,bge_rho.rho());  
@@ -142,19 +144,20 @@ void setJet(PseudoJet &iJet,JetInfo &iJetI,std::vector<PseudoJet> &iParticles, b
     iJetI.pt          = lCorr     .pt();
     iJetI.ptcorr      = iJet      .pt()*lJEC;
     iJetI.ptraw       = iJet      .pt();
-    iJetI.ptclean     = lClean    .pt();
-    iJetI.pttrim      = lTrim     .pt();
-    iJetI.pttrimsafe  = lTrimSafe .pt();
-    iJetI.ptconst     = lConstit  .pt();
+    //iJetI.ptclean     = lClean    .pt();
+    //iJetI.pttrim      = lTrim     .pt();
+    //iJetI.pttrimsafe  = lTrimSafe .pt();
+    //iJetI.ptconst     = lConstit  .pt();
     iJetI.ptunc       = lUnc;
     iJetI.eta         = iJet      .eta();
     iJetI.phi         = iJet      .phi();
     iJetI.mraw        = iJet      .m();
     iJetI.m           = lCorr     .m();
-    iJetI.mclean      = lClean    .m();
-    iJetI.mtrim       = lTrim     .m();
-    iJetI.mtrimsafe   = lTrimSafe .m();
-    iJetI.mconst      = lConstit  .m();
+    //iJetI.mclean      = lClean    .m();
+    //iJetI.mtrim       = lTrim     .m();
+    //iJetI.mtrimsafe   = lTrimSafe .m();
+    //iJetI.mconst      = lConstit  .m();
+    delete area_subtractor;
 }
 
 void setupTree(TTree *iTree,JetInfo &iJet,std::string iName) {
@@ -191,6 +194,7 @@ PseudoJet match(PseudoJet &iJet,vector<PseudoJet> &iJets) {
     }
     return PseudoJet();
 }
+
 void clear(JetInfo &iJet) {
     iJet.pt         = -1;
     iJet.ptraw      = -1;
@@ -208,6 +212,7 @@ void clear(JetInfo &iJet) {
 }
 
 int main (int argc, char ** argv) {
+
   //gROOT->ProcessLine("#include <vector>");          
   int maxEvents     = atoi(argv[1]);
   std::string lName = argv[2];
@@ -215,16 +220,40 @@ int main (int argc, char ** argv) {
   bool        lUseZ = atoi(argv[4]);
   bool        lMet  = atoi(argv[5]);
 
-  //Setup JEC on the fly
-  std::string cmsenv = "/data/nbay04/a/benwu/JetMETTP/CMSSW_6_2_0_SLHC15_fixTk1/src/";
-  std::vector<JetCorrectorParameters> corrParams;
-  corrParams.push_back(JetCorrectorParameters(cmsenv+"BaconProd/Utils/data/Summer13_V1_MC_L1FastJet_AK5PF.txt"));
-  corrParams.push_back(JetCorrectorParameters(cmsenv+"BaconProd/Utils/data/Summer13_V1_MC_L2Relative_AK5PF.txt"));
-  corrParams.push_back(JetCorrectorParameters(cmsenv+"BaconProd/Utils/data/Summer13_V1_MC_L3Absolute_AK5PF.txt"));
+//----------------------------------------------------------------------------
+//  Setup JEC on the fly
+//----------------------------------------------------------------------------
+  std::string cmsenv = getenv("CMSSW_BASE");
+  const std::string globalTag = "DES19_62_V8";
+
+  //AK5PF
+  std::vector<JetCorrectorParameters> PFcorrParams;
+  PFcorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L1FastJet_AK5PF.txt"));
+  PFcorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L2Relative_AK5PF.txt"));
+  PFcorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L3Absolute_AK5PF.txt"));
   //corrParams.push_back(JetCorrectorParameter(cmsenv+'BaconProd/Utils/data/Summer13_V1_DATA_L2L3Residual_AK5PF.txt'));
-  JetCorrectorParameters     param(cmsenv+"BaconProd/Utils/data/Summer13_V1_DATA_Uncertainty_AK5PF.txt");
-  FactorizedJetCorrector   *jetCorr = new FactorizedJetCorrector(corrParams);
-  JetCorrectionUncertainty *jetUnc  = new JetCorrectionUncertainty(param);
+  JetCorrectorParameters     PFparam(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_Uncertainty_AK5PF.txt");
+  FactorizedJetCorrector   *PFjetCorr = new FactorizedJetCorrector(PFcorrParams);
+  JetCorrectionUncertainty *PFjetUnc  = new JetCorrectionUncertainty(PFparam);
+
+  //AK5PFCHS
+  std::vector<JetCorrectorParameters> CHScorrParams;
+  CHScorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L1FastJet_AK5PFchs.txt"));
+  CHScorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L2Relative_AK5PFchs.txt"));
+  CHScorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L3Absolute_AK5PFchs.txt"));
+  //corrParams.push_back(JetCorrectorParameter(cmsenv+'BaconProd/Utils/data/Summer13_V1_DATA_L2L3Residual_AK5PF.txt'));
+  JetCorrectorParameters    CHSparam(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_Uncertainty_AK5PFchs.txt");
+  FactorizedJetCorrector   *CHSjetCorr = new FactorizedJetCorrector(CHScorrParams);
+  JetCorrectionUncertainty *CHSjetUnc  = new JetCorrectionUncertainty(CHSparam);
+
+  //Puppi (L2L3)
+  std::vector<JetCorrectorParameters> PuppicorrParams;
+  PuppicorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L2Relative_AK5PFchs.txt"));
+  PuppicorrParams.push_back(JetCorrectorParameters(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_L3Absolute_AK5PFchs.txt"));
+  //corrParams.push_back(JetCorrectorParameter(cmsenv+'BaconProd/Utils/data/Summer13_V1_DATA_L2L3Residual_AK5PF.txt'));
+  JetCorrectorParameters    Puppiparam(cmsenv+"/src/SubstrAna/Summer14/data/"+globalTag+"_Uncertainty_AK5PFchs.txt");
+  FactorizedJetCorrector   *PuppijetCorr = new FactorizedJetCorrector(PuppicorrParams);
+  JetCorrectionUncertainty *PuppijetUnc  = new JetCorrectionUncertainty(Puppiparam);
 
   //Setup JetAlgos
   double R = 0.4;
@@ -236,124 +265,145 @@ int main (int argc, char ** argv) {
   JetDefinition subjet_def(kt_algorithm,0.2);
   JetCleanser gsn_cleanser(subjet_def,JetCleanser::gaussian_cleansing,JetCleanser::input_nc_separate);
   gsn_cleanser.SetGaussianParameters(0.617,0.62,0.15,0.22);
+
   //Now read a file
   //root://eoscms.cern.ch//store/group/phys_jetmet/ntran/PUPPI/miniSamples/62x/rsgww1000_62x_PU40BX50/ntuple_1_1_VQC.root";
   TTree *lTree = load(lName); 
-  if(lTree->GetEntries() < maxEvents || maxEvents == -1) maxEvents = lTree->GetEntries(); 
+  if(lTree->GetEntries() < maxEvents || maxEvents == -1) 
+    maxEvents = lTree->GetEntries(); 
 
   //Declare Readers
   //fEvt      = new EvtLoader     (lTree);
-  //fMuon     = new MuonLoader    (lTree);
+  fMuon     = new MuonLoader    (lTree);
   fPFCand   = new PFLoader      (lTree,"Puppi_cff.py");
-  //fJet      = new JetLoader     (lTree);
   fGen      = new GenLoader     (lTree);
+  //fJet      = new JetLoader     (lTree);
 
+//----------------------------------------------------------------------------
+//  Setup Output
+//----------------------------------------------------------------------------
   std::size_t pos = lName.find_last_of("/");
   std::string outfile = lName.substr(pos+1, lName.size() - pos);
+  outfile = "Puppi_" +outfile;
   TFile *lFile = new TFile(outfile.c_str(),"RECREATE");
   TTree *lOut  = new TTree("Tree","Tree");
+
   //Setup Tree
   //fEvt    ->setupTree      (lOut);
-  //fMuon   ->setupTree      (lOut);
+  fMuon   ->setupTree      (lOut);
   fPFCand ->setupTree      (lOut);
-  //if(lGen) fGen ->setupTree      (lOut);
+  if(lGen) 
+    fGen ->setupTree      (lOut);
 
-  int lIndex = 0; lOut->Branch("index",&lIndex,"lIndex/F");
+  int lIndex = 0; 
+  lOut->Branch("index",&lIndex,"lIndex/F");
+
   JetInfo JGen;     setupTree(lOut,JGen    ,"Gen"  );
   JetInfo JPF;      setupTree(lOut,JPF     ,"PF"   );
   JetInfo JPup;     setupTree(lOut,JPup    ,"Puppi");
   JetInfo JCHS;     setupTree(lOut,JCHS    ,"CHS"  );
-  JetInfo JCHS2GeV; setupTree(lOut,JCHS2GeV,"CHS2GeV");
-  JetInfo JSoft;    setupTree(lOut,JSoft   ,"SK"  );
-  JetInfo JSoftCHS; setupTree(lOut,JSoftCHS,"SKCHS");
+  //JetInfo JCHS2GeV; setupTree(lOut,JCHS2GeV,"CHS2GeV");
+  //JetInfo JSoft;    setupTree(lOut,JSoft   ,"SK"  );
+  //JetInfo JSoftCHS; setupTree(lOut,JSoftCHS,"SKCHS");
+
   std::vector<TLorentzVector> lVetoes;
-  for(int i0 = 0; i0 < maxEvents; i0++) { 
+  for(int i0 = 0; i0 < maxEvents; i0++)
+  { 
     lVetoes.clear();
-    //if(i0 < 108) continue;
-    if(i0 % 100 == 0) std::cout << "===> Processed " << i0 << " - Done : " << (float(i0)/float(maxEvents)) << std::endl;
+    if(i0 % 500 == 0) 
+      std::cout << "===> Processed " << i0 << " - Done : " << (float(i0)/float(maxEvents)) << std::endl;
+
     clear(JGen);
     clear(JPF);
     clear(JPup);
     clear(JCHS);
-    clear(JCHS2GeV);
-    //fMuon   ->load(i0); 
+    //clear(JCHS2GeV);
+
     //fJet    ->load(i0); 
+    fMuon   ->load(i0); 
     fGen    ->load(i0);
-    //bool lFindZ = fMuon->selectZ(lVetoes);
-    //if(!lFindZ && lUseZ) continue;
+    bool lFindZ = fMuon->selectZ(lVetoes);
+    if(!lFindZ && lUseZ) continue;
     if(lUseZ) continue;
 
-    vector<PseudoJet> genJets;
     vector<PseudoJet> gen_event;
     gen_event  = fGen   ->genFetch();
-    ClusterSequenceArea pGen(gen_event    ,jet_def,area_def);
-    genJets     = sorted_by_pt(pGen    .inclusive_jets());
     //bool lFind20Jet = (genJets[2].pt() > 20);
     //if(!lFind20Jet) continue;
     //////////////////////////////////////////////////////
-    //TLorentzVector lZ = fMuon->boson();
-    TLorentzVector lZ(0, 0, 0, 0);
+    TLorentzVector lZ = fMuon->boson();
+    //TLorentzVector lZ(0, 0, 0, 0);
     fPFCand->load(i0,lZ);
     vector<PseudoJet> puppi_event     = fPFCand->puppiFetch(lZ);
     if(lMet) lOut->Fill();
     if(lMet) continue;
     vector<PseudoJet> pf_event        = fPFCand->pfFetch();
     vector<PseudoJet> chs_event       = fPFCand->pfchsFetch(-1);
-    vector<PseudoJet> chs_event2GeV   = fPFCand->pfchsFetch( 2.);
-    //////////////////////////////////////////////////////
-    SoftKiller soft_killer   (0.4,0.4);
-    SoftKiller soft_killerCHS(4.0,0.5, !SelectorIsPupCharged());
-    vector<PseudoJet> soft_event    = soft_killer   (pf_event);
-    vector<PseudoJet> softCHS_event = soft_killerCHS(chs_event);
+    //vector<PseudoJet> chs_event2GeV   = fPFCand->pfchsFetch( 2.);
 
-    //ClusterSequenceArea pGen    (gen_event    ,jet_def,area_def);
+    ClusterSequenceArea pGen    (gen_event    ,jet_def,area_def);
     ClusterSequenceArea pPup    (puppi_event  ,jet_def,area_def);
     ClusterSequenceArea pPF     (pf_event     ,jet_def,area_def);
     ClusterSequenceArea pCHS    (chs_event    ,jet_def,area_def);
-    ClusterSequenceArea pCHS2GeV(chs_event2GeV,jet_def,area_def);
-    ClusterSequenceArea pSoft   (soft_event   ,jet_def,area_def);
-    ClusterSequenceArea pSoftCHS(softCHS_event,jet_def,area_def);
+    //ClusterSequenceArea pCHS2GeV(chs_event2GeV,jet_def,area_def);
 
+    vector<PseudoJet> genJets     = sorted_by_pt(pGen    .inclusive_jets());
     vector<PseudoJet> puppiJets   = sorted_by_pt(pPup    .inclusive_jets());
     vector<PseudoJet> pfJets      = sorted_by_pt(pPF     .inclusive_jets());
     vector<PseudoJet> chsJets     = sorted_by_pt(pCHS    .inclusive_jets());
-    vector<PseudoJet> chs2GeVJets = sorted_by_pt(pCHS2GeV.inclusive_jets());
-    vector<PseudoJet> softJets    = sorted_by_pt(pSoft   .inclusive_jets());
-    vector<PseudoJet> softCHSJets = sorted_by_pt(pSoftCHS.inclusive_jets());
+    //vector<PseudoJet> chs2GeVJets = sorted_by_pt(pCHS2GeV.inclusive_jets());
+
+    //////////////////////////////////////////////////////
+    //SoftKiller soft_killer   (0.4,0.4);
+    //SoftKiller soft_killerCHS(4.0,0.5, !SelectorIsPupCharged());
+    //vector<PseudoJet> soft_event    = soft_killer   (pf_event);
+    //vector<PseudoJet> softCHS_event = soft_killerCHS(chs_event);
+    //ClusterSequenceArea pSoft   (soft_event   ,jet_def,area_def);
+    //ClusterSequenceArea pSoftCHS(softCHS_event,jet_def,area_def);
+    //vector<PseudoJet> softJets    = sorted_by_pt(pSoft   .inclusive_jets());
+    //vector<PseudoJet> softCHSJets = sorted_by_pt(pSoftCHS.inclusive_jets());
+
     vector<PseudoJet> loopJets = pfJets;
     if(lGen) loopJets = genJets;
-    for(unsigned int i0 = 0; i0 < loopJets.size(); i0++) {
-      if(loopJets[i0].pt() < 20) continue;
+
+    for(unsigned int i0 = 0; i0 < loopJets.size(); i0++) 
+    {
       lIndex = i0;
       bool lMatch = false;
-      for(unsigned int i1 = 0; i1 < lVetoes.size(); i1++) { 
+      for(unsigned int i1 = 0; i1 < lVetoes.size(); i1++) 
+      { 
         double pDPhi = fabs(lVetoes[i1].Phi()-loopJets[i0].phi());
         if(pDPhi > TMath::Pi()*2.-pDPhi) pDPhi = TMath::Pi()*2.-pDPhi;
         double pDEta = fabs(lVetoes[i1].Eta()-loopJets[i0].eta());
         if(sqrt(pDPhi*pDPhi+pDEta*pDEta)  < 0.5) lMatch = true;
       }
       if(lMatch) continue;
-      if(loopJets[i0].pt() < 20) continue;
-      PseudoJet puppiJet   = match(loopJets[i0],puppiJets);
+      if(loopJets[i0].pt() < 1) continue;
+      PseudoJet genJet     = match(loopJets[i0],genJets);
       PseudoJet pfJet      = match(loopJets[i0],pfJets   );
       PseudoJet chsJet     = match(loopJets[i0],chsJets  );
-      PseudoJet chs2GeVJet = match(loopJets[i0],chs2GeVJets);
-      PseudoJet softJet    = match(loopJets[i0],softJets);
-      PseudoJet softCHSJet = match(loopJets[i0],softCHSJets);
-      PseudoJet genJet     = match(loopJets[i0],genJets);
-      std::cout << "==> " << genJet.pt() << " -- " << genJets[i0].pt() << " -- " << loopJets[i0].pt() << std::endl;
-      if(genJet.pt()     != 0) setJet(genJet,     JGen    ,gen_event    ,false,jetCorr,jetUnc,gsn_cleanser);
-      if(pfJet.pt()      != 0) setJet(pfJet ,     JPF     ,pf_event     ,false,jetCorr,jetUnc,gsn_cleanser);
-      if(chsJet.pt()     != 0) setJet(chsJet,     JCHS    ,chs_event    ,true ,jetCorr,jetUnc,gsn_cleanser);
-      if(chs2GeVJet.pt() != 0) setJet(chs2GeVJet, JCHS2GeV,chs_event2GeV,true ,jetCorr,jetUnc,gsn_cleanser);
-      if(puppiJet.pt()   != 0) setJet(puppiJet  , JPup    ,puppi_event  ,true ,jetCorr,jetUnc,gsn_cleanser);
-      if(softJet.pt()    != 0) setJet(softJet   , JSoft   ,soft_event   ,false,jetCorr,jetUnc,gsn_cleanser);
-      if(softCHSJet.pt() != 0) setJet(softCHSJet, JSoftCHS,softCHS_event,true ,jetCorr,jetUnc,gsn_cleanser);
+      PseudoJet puppiJet   = match(loopJets[i0],puppiJets);
+      //PseudoJet chs2GeVJet = match(loopJets[i0],chs2GeVJets);
+      //std::cout << "==> " << genJet.pt() << " -- " << genJets[i0].pt() << " -- " << loopJets[i0].pt() << std::endl;
+      if(genJet.pt()     != 0) setJet(genJet    , JGen    , gen_event    , false, PFjetCorr   , PFjetUnc   , gsn_cleanser);
+      if(pfJet.pt()      != 0) setJet(pfJet     , JPF     , pf_event     , false, PFjetCorr   , PFjetUnc   , gsn_cleanser);
+      if(chsJet.pt()     != 0) setJet(chsJet    , JCHS    , chs_event    , true , CHSjetCorr  , CHSjetUnc  , gsn_cleanser);
+      //if(chs2GeVJet.pt() != 0) setJet(chs2GeVJet, JCHS2GeV, chs_event2GeV, true , CHSjetCorr  , CHSjetUnc  , gsn_cleanser);
+      if(puppiJet.pt()   != 0) setJet(puppiJet  , JPup    , puppi_event  , true , PuppijetCorr, PuppijetUnc, gsn_cleanser);
+
+      //////////////////////////////////////////////////////
+      //PseudoJet softJet    = match(loopJets[i0],softJets);
+      //PseudoJet softCHSJet = match(loopJets[i0],softCHSJets);
+      //if(softJet.pt()    != 0) setJet(softJet   , JSoft   , soft_event   , false, PFjetCorr   , PFjetUnc   , gsn_cleanser);
+      //if(softCHSJet.pt() != 0) setJet(softCHSJet, JSoftCHS, softCHS_event, true , PFjetCorr   , PFjetUnc   , gsn_cleanser);
+
       lOut->Fill();
     }
   }
   lFile->cd();
   lOut->Write();
+  lFile->Close();
+
   //delete FactorizedJetCorrector;
   //delete JetCorrectionUncertainty;
 }  
